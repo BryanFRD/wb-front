c6637e8274f02d916844a50ad6de68154f78d898
Bryan <bryanferrando59@gmail.com>
2023-05-28 20:29:22 +0200

    Add chart for measures

--- sensor/index.js
+++ sensor/index.js
@@ -1,31 +1,41 @@
 import { API_URL } from '../js/constant.js';
 
 const id = new URLSearchParams(window.location.search).get('id');
+const chart = new Chart(document.getElementById('measureChart'), 
+  {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [{}],
+    },
+    options: {
+      responsive: true,
+      scales: {
+        x: {
+          reverse: true,
+          display: true,
+          title: {
+            display: true,
+            text: 'Temps'
+          }
+        },
+        y: {
+          display: true,
+          title: {
+            display: true,
+            text: 'Mesure'
+          }
+        },
+      }
+    }
+  });
 
 setInterval(() => {
   updateGraph();
+  updateSensor();
 }, 5000);
 
-const measureGraph = (data) => {
-  const config = {
-    type: 'bar',
-    data,
-    options: {
-      scales: {
-        x: {
-          type: 'time'
-        },
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  }
-  return config;
-}
-
 const fetchMeasure = async () => {
-  const id = new URLSearchParams(window.location.search).get('id');
   return await fetch(`${API_URL}/sensors/${id}/measurements`)
     .then(resp => resp.json())
     .then(resp => resp)
@@ -36,16 +46,52 @@ const fetchMeasure = async () => {
 }
 
 const updateGraph = async () => {
-  const sensors = await fetchMeasure();
-  console.log('sensors:', sensors);
+  const measurements = await fetchMeasure();
   
-  const sensorsDiv = document.getElementById('sensors');
-  sensorsDiv.textContent = '';
-  sensors?.datas?.forEach(sensor => {
-    sensorsDiv.appendChild(measureGraph(sensor));
+  chart.data.labels = measurements?.datas?.map(value => {
+    const date = new Date(value.createdAt.date);
+    return date.toLocaleTimeString('fr-FR', {hour: '2-digit', minute: '2-digit', second: '2-digit'});
   });
   
-  const chart = new Chart(document.getElementById('sensorChart'), measureGraph(sensors?.data));
+  chart.data.datasets[0].data = measurements?.datas?.map(value => value.measure)
+  
+  chart.update();
 }
 
+const fetchSensor = async () => {
+  return await fetch(`${API_URL}/sensors/${id}`)
+    .then(resp => resp.json())
+    .then(resp => resp)
+    .catch(error => {
+      console.log('error:', error);
+      return null;
+    });
+}
+
+const updateSensor = async () => {
+  const sensor = await fetchSensor();
+  
+  const moduleDiv = document.getElementById('sensorDetails');
+  moduleDiv.textContent = '';
+  
+  const titleElement = document.createElement('h2');
+  moduleDiv.append(titleElement);
+  
+  if(!sensor?.id){
+    titleElement.innerText = `Impossible de trouver un capteur avec l'id : ${id}`;
+    return;
+  }
+  
+  const measurementTypeElement = document.createElement('h2');
+  measurementTypeElement.innerText = `Mesure : ${sensor.measurementType}`;
+  
+  const statusElement = document.createElement('h2');
+  statusElement.setAttribute('class', sensor.status === 'active' ? 'text-success' : sensor.status === 'inactive' ? 'text-danger' : 'text-warning');
+  statusElement.innerText = `(${sensor.status})`;
+  
+  titleElement.innerText = sensor.name;
+  moduleDiv.append(measurementTypeElement, statusElement);
+}
+
+updateSensor();
 updateGraph();
