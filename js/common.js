065e7022c5423acd7ef8b11425a5cf67998e5a59
Bryan <bryanferrando59@gmail.com>
2023-05-29 08:22:41 +0200

    Add navbar button event and toast

--- js/common.js
+++ js/common.js
@@ -1,3 +1,5 @@
+import { moduleModal, sensorModal } from './component.js';
+
 searchBarForm?.addEventListener('submit', (event) => {
   event.preventDefault();
   
@@ -8,3 +10,32 @@ searchBarForm?.addEventListener('submit', (event) => {
     window.location.href = `${window.location.origin}/?${searchParams.toString()}`;
   }
 });
+
+export const modalBootstrap = new bootstrap.Modal('#modal');
+
+document.getElementById("createModuleButton").addEventListener('click', () => {
+  moduleModal(modalBootstrap);
+  modalBootstrap.show();
+});
+
+document.getElementById("createSensorButton").addEventListener('click', () => {
+  sensorModal(modalBootstrap);
+  modalBootstrap.show();
+});
+
+document.getElementById("createMeasureButton").addEventListener('click', () => {
+  moduleModal(modalBootstrap);
+  modalBootstrap.show();
+});
+
+export const toastBootstrap = new bootstrap.Toast('#toast');
+
+export const toast = (message, className = 'text-bg-secondary') => {
+  document.getElementById('toast').setAttribute('class', `${className} toast align-items-center border-0 position-fixed bottom-0 end-0 m-3`);
+  
+  if(toastBootstrap.isShown())
+    toastBootstrap.dispose();
+  
+  document.getElementById("toastBody").innerText = message;
+  toastBootstrap.show();
+}
